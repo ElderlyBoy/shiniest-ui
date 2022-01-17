b1b90c9845f1a667dfeccf9e95a28b0125d1366a
XingYu Xiao <shiniestcn@icloud.com>
2022-01-17 09:23:25 +0800

    文件调整

--- example/views/Home.vue
+++ example/views/Home.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="home" style="padding: 100px;">
-    <sh-button size="large" @click="click">Hello World</sh-button>
+    <sh-button type="">Hello World</sh-button>
+    <sh-button type="primary">Hello World</sh-button>
+    <sh-button type="success">Hello World</sh-button>
+    <sh-button type="warning">Hello World</sh-button>
+    <sh-button type="danger">Hello World</sh-button>
+    <sh-button type="info">Hello World</sh-button>
     <i class="sh-icon-error">123</i>
   </div>
 </template>
