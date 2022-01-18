c2e6783fc476b3300fba51502261ed2c7bb108a7
XingYu Xiao <shiniestcn@icloud.com>
2022-01-18 11:39:26 +0800

    停止跟踪iconfont.js、src/index.js(自动生成)

--- iconfont.js
+++
@@ -1,9 +0,0 @@
-/* auto build by build/build.iconfont.js */
-export default [
-  "sh-icon-chenggong",
-  "sh-icon-error",
-  "sh-icon-guanyu",
-  "sh-icon-jingshi",
-  "sh-icon-success",
-  "sh-icon-tishi"
-]
