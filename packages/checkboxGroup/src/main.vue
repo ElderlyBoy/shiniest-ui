b11cae4a0607c0d45ebe3b537e3fe64d9cbc4125
XingYu Xiao <shiniestcn@icloud.com>
2022-01-24 03:33:17 +0800

    checkboxGroup组件

---
+++ packages/checkboxGroup/src/main.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="sh-checkbox-group">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'ShCheckboxGroup',
+    model: {
+      prop: 'bindValue',
+      event: 'change'
+    },
+    props: {
+      bindValue: { type: Array, required: true },
+      disabled: { type: Boolean, default: false }
+    },
+    provide(){
+      return {
+        shCheckboxGroup: this
+      }
+    },
+    watch: {
+      bindValue: {
+        deep: true,
+        handler(val){
+          this.$emit('change', val)
+        }
+      }
+    },
+    methods: {
+      
+    }
+  }
+</script>
