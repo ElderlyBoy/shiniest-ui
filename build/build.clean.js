3494c9d7c4579559466ac7c177b15d3ebbcc913d
XingYu Xiao <shiniestcn@icloud.com>
2022-01-25 00:13:30 +0800

    优化clean算法

--- build/build.clean.js
+++ build/build.clean.js
@@ -1,10 +1,16 @@
 const rimraf = require('rimraf');
 const path = require('path');
 
-rimraf.sync(path.resolve(__dirname, '../lib'))
-rimraf.sync(path.resolve(__dirname, '../dist'))
-rimraf.sync(path.resolve(__dirname, '../src/index.js'))
-rimraf.sync(path.resolve(__dirname, '../packages/stylesheet/fonts/*'))
-rimraf.sync(path.resolve(__dirname, '../iconfont.js'))
+const pathList = [
+  '../lib',
+  '../dist',
+  '../src/index.js',
+  '../packages/stylesheet/fonts/*',
+  '../iconfont.js'
+]
+
+pathList.forEach(p => {
+  rimraf.sync(path.resolve(__dirname, p))
+})
 
 console.log('运行clean命令完成！');
