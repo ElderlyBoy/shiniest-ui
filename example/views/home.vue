44179b02b8d1aa7ca20ace1a5c91c6b302df8f06
XingYu Xiao <shiniestcn@icloud.com>
2022-02-09 23:38:12 +0800

    rename Home => home

---
+++ example/views/home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <h1>Shiniest UI</h1>
+    <h2>Let Develop More Sample.</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+<style lang="scss">
+  .home {
+    padding: 10%;
+  }
+</style>
