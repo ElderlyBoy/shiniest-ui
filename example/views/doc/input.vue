354cb8c2a26457b5b164c5be3d6abc7d3f95dca6
XingYu Xiao <shiniestcn@icloud.com>
2022-02-10 20:39:31 +0800

    优化

--- example/views/doc/input.vue
+++ example/views/doc/input.vue
@@ -2,6 +2,8 @@
   <div class="input-box" style="width: 300px;">
     <h3>基本用法</h3>
     <sh-input v-model="value"></sh-input>
+    <h3>可清空</h3>
+    <sh-input clearable v-model="value"></sh-input>
     <h3>插槽</h3>
     <sh-input v-model="value">
       <template #prepend>
@@ -10,7 +12,7 @@
       <template #append>搜索</template>
     </sh-input>
     <h3>字数限制</h3>
-    <sh-input v-model="value" maxlength="30" show-world-limit></sh-input>
+    <sh-input clearable v-model="value" maxlength="30" show-world-limit></sh-input>
     <h3>文本域</h3>
     <sh-input :auto-resize="{max: 10, min: 5}" type="textarea" v-model="value" maxlength="300"></sh-input>
   </div>
