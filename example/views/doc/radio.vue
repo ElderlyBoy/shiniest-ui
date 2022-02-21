5cc1079493fa79ede32fa206a4658f66ab83269d
XingYu Xiao <shiniestcn@icloud.com>
2022-02-22 02:25:19 +0800

    radio文档优化

--- example/views/doc/radio.vue
+++ example/views/doc/radio.vue
@@ -7,7 +7,7 @@
     <sh-radio disabled @change="change" v-model="radio" value="hello">hello</sh-radio>
     <sh-radio disabled v-model="radio" value="world">world</sh-radio>
     <h3>单选框组</h3>
-    <sh-radio-group disabled @change="change" v-model="group">
+    <sh-radio-group @change="change" v-model="group">
       <sh-radio v-for="num in 5" :key="num" :value="num">选项{{num}}</sh-radio>
     </sh-radio-group>
   </div>
