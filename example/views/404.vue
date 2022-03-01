538b4a488eb0c8573050dcd39847d922fd3f2ca3
XingYu Xiao <shiniestcn@icloud.com>
2022-03-01 08:47:49 +0800

    404暂时优化

--- example/views/404.vue
+++ example/views/404.vue
@@ -1,5 +1,13 @@
 <template>
-   <div><h1>404</h1><h2>Page Not Found</h2></div>
+   <div>
+      <h1>404</h1>
+      <h2>Page Not Found</h2>
+      <center>
+         <a @click="$router.go(-2)">返回上一页</a>
+         <router-link to="/doc">返回文档页</router-link>
+         <router-link to="/">返回首页</router-link>
+      </center>
+   </div>
 </template>
 
 <script>
