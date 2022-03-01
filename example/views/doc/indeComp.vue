62ffff206fe941187ba3fc7acbca1d447754502a
XingYu Xiao <shiniestcn@icloud.com>
2022-03-01 08:50:17 +0800

    自定义动态示例组件

---
+++ example/views/doc/indeComp.vue
@@ -0,0 +1,18 @@
+<template>
+   <div class="checkbox-box">
+      <div class="example">
+         <div class="example-item">
+            <h2>{{name}} 动态示例</h2>
+            <slot name="options"></slot>
+         </div>
+      </div>
+      <slot></slot>
+   </div>
+</template>
+
+<script>
+   export default {
+      name: 'IndeComp',
+      props: ['name']
+   }
+</script>
